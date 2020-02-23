544083743da49e81ac781c0e65b2481e35d1f4c4
Mikołaj Dębicki <mikolaj.debicki@opus.audio>
2020-02-23 16:01:20 +0400

    ER fixed

--- createRawSchema.sql
+++ createRawSchema.sql
@@ -4,5 +4,5 @@ create table Pub (k text, p text);
 
 drop table if exists Field cascade;
 create table Field (k text, i text, p text, v text);
-copy Pub from '/Users/andriylunin/Documents/CS_work/DBMS/PS1/pubFile.txt';
-copy Field from '/Users/andriylunin/Documents/CS_work/DBMS/PS1/fieldFile.txt';
+copy Pub from '/Users/mikolajdebicki/Desktop/super-octo-engine/pubFile.txt';
+copy Field from '/Users/mikolajdebicki/Desktop/super-octo-engine/fieldFile.txt';
