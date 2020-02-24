4dadc03669e7df87b9b9107c3ecb9e4d9fbd329d
Mikołaj Dębicki <mikolaj.debicki@opus.audio>
2020-02-24 15:19:46 +0400

    andriy being dumb again

--- solution-raw.sql
+++ solution-raw.sql
@@ -11,3 +11,8 @@ select f from (select distinct Field.p as f, Pub.p as p from field inner join pu
  title
  year
 (5 rows)*/
+
+create index idx_pub on Pub (k);
+create index idx_field on Field (k);
+
+	
