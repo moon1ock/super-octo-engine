0ac8f16e9feb035568c58d14c8336c3102e4afc6
Andriy <lunin.andriy@gmail.com>
2020-02-24 20:22:02 +0400

    progress on 3

--- transform.sql
+++ transform.sql
@@ -10,13 +10,14 @@ insert into tmpAuthors (name, key) select v,k from Field f where f.p = 'author';
 
 create table tmpHomepages (homepage text, key text);
 
-insert into tmpHomepages (homepage, key) select v,k from Field f where f.p = 'url' and f.k like 'homepage%';
+insert into tmpHomepages (key, homepage) select k, v from Field f where f.p = 'url' and f.k like 'homepage%';
 
 -- create table authors and concatenate the tmp tables to get the actual set of authors
 
-create table author (id int primary key, name text, homepage text);
+create table authors (id int primary key, name text, homepage text);
+
+insert into authors (name, homepage) select tmpauthors.name, tmphomepages.homepage from tmpAuthors left outer join tmphomepages limit 10;
 
 
 
-
-
+-- we know that every author is uniquely identified by their homepage! so we'll just need the id mapped from homepage and pubid to make the Authored statement
