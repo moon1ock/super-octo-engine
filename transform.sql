cc93fbfe4c392fdc92b4fe994cfec6c409e9e3a7
Mikołaj Dębicki <mikolaj.debicki@opus.audio>
2020-02-24 16:28:18 +0400

    start of q3

--- transform.sql
+++ transform.sql
@@ -1,1 +1,7 @@
-insert into author
+insert into author values ((select v from Field where p = 'author') as "name", (select z.v from Pub x, Field y, Field z where x.k=y.k and y.k=z.k and x.p='www' and y.v= name and z.p = 'url') as "homepage");
+
+
+
+
+
+3.4
