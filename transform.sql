c86a73893b8ece39171f64cfd02303250f2ef14b
Andriy <lunin.andriy@gmail.com>
2020-02-24 19:43:27 +0400

    backup on q3

--- transform.sql
+++ transform.sql
@@ -1,12 +1,22 @@
-insert into author (select y.v, z.v from Pub x, Field y, Field z where y.p = 'author' and x.k=y.k and y.k=z.k and x.p='www' and y.v= name and z.p = 'url');
+-- here have a table author | key
+create table tmpAuthors (name text, key text);
+
+insert into tmpAuthors (name, key) select v,k from Field f where f.p = 'author';
+
+
+
+
+-- have a table key | homepage
+
+create table tmpHomepages (homepage text, key text);
+
+insert into tmpHomepages (homepage, key) select v,k from Field f where f.p = 'url' and f.k like 'homepage%';
+
+-- create table authors and concatenate the tmp tables to get the actual set of authors
+
+create table author (id int primary key, name text, homepage text);
 
 
 
 
 
-
-select v from Field where p = 'author',
-	select z.v from Pub x, Field y, Field z where x.k=y.k and y.k=z.k and x.p='www' and y.v= name and z.p = 'url'
-
-insert into author (name, homepage)
-select 
