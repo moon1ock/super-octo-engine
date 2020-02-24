74cef49e8230479857379f098b7e0cfd917232af
Mikołaj Dębicki <mikolaj.debicki@opus.audio>
2020-02-24 14:50:06 +0400

    Q2 solved

--- solution-raw.sql
+++ solution-raw.sql
@@ -1,1 +1,13 @@
 select p, count(*) from Pub p group by p.p;
+
+
+
+select f from (select distinct Field.p as f, Pub.p as p from field inner join pub on Field.k=Pub.k) as Groups group by f having count(f)=8;
+/*   f    
+--------
+ author
+ ee
+ note
+ title
+ year
+(5 rows)*/
