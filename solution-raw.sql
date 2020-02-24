577f034271c0a5cf23a3cc8d7532d9c7bd6f5c93
Andriy <lunin.andriy@gmail.com>
2020-02-24 15:16:07 +0400

    Q1

--- solution-raw.sql
+++ solution-raw.sql
@@ -1,1 +1,19 @@
+
 select p, count(*) from Pub p group by p.p;
+
+
+/*
+       p       |  count
+---------------+---------
+ article       | 2203997
+ book          |   17760
+ incollection  |   60493
+ inproceedings | 2553803
+ mastersthesis |      12
+ phdthesis     |   75524
+ proceedings   |   43392
+ www           | 2451080
+(8 rows) */
+
+
+
