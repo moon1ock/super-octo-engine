ced0af4c6890346677f77473fff418c072f1ba6a
Andriy <lunin.andriy@gmail.com>
2020-02-25 11:46:16 +0400

    ...

--- transform.sql
+++ transform.sql
@@ -29,8 +29,33 @@ alter table authors add constraint authors_pk primary key (id);
 
 -- deal with publication
 
-create table publications (pubid numeric unique not null, pubkey text not null unique, title text not null, year int not null);
+
+/* insert into publications(pubkey) select distinct k from Field; */
+
+/* insert into publications (title) select f.v from Field f where f.p = 'title' and publications.pubkey = f.k; */
 
 
+create table keys (pk text); -- have a table of unique distinct keys
+insert into keys (pk) select distinct k from Field; -- populate the table
+
+create table tmpublyear (pk text, year int); -- table of key | yaar
+-- use these tables to full outer join with the other ones and have key | year | publications
+
+insert into tmpublyear (pk, year) select distinct pk, cast(v as int) from Field f, Keys k where k.pk = f.k and f.p = 'year';
+
+create table tmpubltitle (pk text, title text);
+insert into tmpubltitle (pk, title) select distinct pk, v from Field f, Keys k where k.pk = f.k and f.p = 'title';
+
+
+create table publications (pubkey text not null, title text, year int); -- use this table to store the full join of the above two
+
+
+insert into publications (pubkey, year, title) select tmpubltitle.pk, tmpublyear.year, tmpubltitle.title from tmpublyear full outer join tmpubltitle on tmpubltitle.pk = tmpublyear.pk where tmpublyear.year is NOT NULL and tmpubltitle.title IS NOT NULL;
+-- after this we have a table of publications, their years, and respective keys
+-- due to a bug in the initial database, we decided to delete ` books/daglib/0079308 ` record, as it repeats twice and compromises the uniqueness of keys
+
+delete from publications where pubkey = 'books/daglib/0079308'; -- now, lets impose the constraint of uniqueness onto publications
+
+alter table publications add constraint 
 
 
