24cea02580d98cd9a5a4304874060d82ebb0aef1
Mikołaj Dębicki <mikolaj.debicki@opus.audio>
2020-02-25 23:24:52 +0400

    I almost have spoken

--- vis.sql
+++ vis.sql
@@ -9,4 +9,15 @@ insert into answer (name, cnt) select name, num from tmppub left outer join auth
 \copy (select distinct name,cnt from answer order by cnt desc) To '/Users/mikolajdebicki/Desktop/super-octo-engine/pub.csv' With CSV
 drop table tmppub, answer;
 
+
+
+create table colab (id numeric, cnt numeric);
+insert into colab (id, cnt) select i.id, count(distinct c.id) from authored i, authored c where i.id != c.id and i.pubid = c.pubid group by i.id;
+
+create table topcolabs (name text, cnt numeric);
+insert into topcolabs (name, cnt) select distinct authors.name, colab.cnt from authors left join colab on authors.id=colab.id where authors.name is not NULL and colab.cnt is not null order by colab.cnt desc;
+
+\copy (select * from topcolabs) To '/Users/mikolajdebicki/Desktop/super-octo-engine/col.csv' With CSV
+drop table colab, topcolabs;
+
 -- Both plots decay exponentially
